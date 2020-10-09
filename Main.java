9627fe9ba384bae9ff95fcbd2268b5d70fe5ff26
quigt7936 <71140817+quigt7936@users.noreply.github.com>
2020-10-09 09:17:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,38 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program sorts an array of 10 integers
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // make an array
+    int[] sortingNumbers = new int[10];
+
+    // asks for 10 integers to put in the array
+    System.out.println("Please enter in 10 integers to put into the array");
+    for (int i = 0; i < 10; i++) {
+      sortingNumbers[i] = input.nextInt();
+    }
+
+    // sort the array
+    for (int i = 0; i < (sortingNumbers.length - 1); i++) {
+      for (int j = i+1; j < sortingNumbers.length; j++) {
+        if (sortingNumbers[i] > sortingNumbers[j]) {
+          // swaps the 2 values by making a circle of swaps
+          int swapNumber = sortingNumbers[i];
+          sortingNumbers[i] = sortingNumbers[j];
+          sortingNumbers[j] = swapNumber;
+        }
+      }
+    }
+
+    // print the array one element at a time, the last one doesn't have a comma
+    System.out.println("The array sorted in ascending order");
+    for (int i = 0; i < 9; i++) {
+      System.out.print(sortingNumbers[i] + ", ");
+    }
+    System.out.println(sortingNumbers[sortingNumbers.length - 1]);
     
   }
 }
